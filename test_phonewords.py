1c1c82569498c50ef9f534aa138dc55d20c82b32
Adrian Nettleship <nettlesh@amazon.com>
2023-01-03 22:28:35 +0000

    T test_phonewords_should_return_empty_list_when_given_empty_list_and_any_number

--- test_phonewords.py
+++ test_phonewords.py
@@ -1,3 +1,4 @@
+import pytest
 from phonewords import phonewords
 
 def test_phonewords_should_return_empty_list_when_given_empty_list_and_blank_number():
@@ -5,7 +6,14 @@ def test_phonewords_should_return_empty_list_when_given_empty_list_and_blank_num
     wordlist = []
     assert phonewords(number, wordlist) == [] 
 
+
 def test_phonewords_should_return_empty_list_when_given_any_list_and_blank_number():
     number = ""
     wordlist = ["a"]
     assert phonewords(number, wordlist) == []
+
+
+@pytest.mark.parametrize("number", [(""),("1"),("12345"),("987654321")])
+def test_phonewords_should_return_empty_list_when_given_empty_list_and_any_number(number):
+    wordlist = []
+    assert phonewords(number, wordlist) == [] 
