ef4e0a138193b9d2333fbd7e66af9ba73e9ff7af
a.nettleship@gmail.com <a.nettleship@gmail.com>
2023-01-27 12:40:14 +0000

    refactor to build set for number

--- phonewords.py
+++ phonewords.py
@@ -3,24 +3,32 @@ import typing
 class phonewords:
     def __init__(self):
         self.lookup = {
+            "1" : {"d","e","f"},
             "2" : {"a","b","c"},
             "3" : {"d","e","f"},
+            "4" : {"d","e","f"},
+            "5" : {"d","e","f"},
+            "6" : {"d","e","f"},
+            "7" : {"d","e","f"},
+            "8" : {"d","e","f"},
+            "9" : {"d","e","f"},
+            "0" : {"d","e","f"},
         }
         
 
     def list_words(self, numstring: str, wordlist: list) -> list:
 
-        if len(numstring) == 0:
-            return []
-
+              
         result = []
 
+        this_lookup = set()
+
+        for num in numstring:
+            this_lookup.update(self.lookup[num])
+
         for word in wordlist:
-            if word in self.lookup["2"]:
+            if word in this_lookup: 
                 result.append(word)
-            if len(numstring) > 1:
-                if word in self.lookup["3"]:
-                    result.append(word)
 
 
         return result 
