eec0843093fa5f7a6df058a07f0358b1dbd71338
a.nettleship@gmail.com <a.nettleship@gmail.com>
2023-01-27 12:17:22 +0000

    refactor to use lookup

--- phonewords.py
+++ phonewords.py
@@ -1,16 +1,22 @@
 import typing
 
 class phonewords:
+    def __init__(self):
+        self.lookup = {
+            "2" : {"a","b","c"},
+        }
+        
+
     def list_words(self, numstring: str, wordlist: list) -> list:
 
         if len(numstring) == 0:
             return []
 
-        if numstring == "2" and "a" in wordlist:
-            return ["a"]
+        result = []
 
-        if numstring == "2" and "b" in wordlist:
-            return ["b"]
+        for word in wordlist:
+            if word in self.lookup["2"]:
+                result.append(word)
 
 
-        return wordlist 
+        return result 
