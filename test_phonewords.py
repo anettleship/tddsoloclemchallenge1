dd06eff6492f595517fb95c7e7ab7df0b448fc2a
a.nettleship@gmail.com <a.nettleship@gmail.com>
2023-01-27 12:27:24 +0000

    T refactor and add test_phonewords_should_return_both_items_when_numstring_is_two_digits_and_word_list_contains_two_items_one_matching_each_number

--- test_phonewords.py
+++ test_phonewords.py
@@ -33,10 +33,19 @@ def test_phonewords_should_return_list_when_wordlist_is_single_letter_and_number
 input_sample = [
     ("2", ["a", "d"], ["a"]),
     ("2", ["b", "d"], ["b"]),
-    ("23", ["b", "d"], ["b", "d"]),
 ]
 
 @pytest.mark.parametrize("number,wordlist,expected", input_sample)
 def test_phonewords_should_return_one_item_when_wordlist_contains_one_matching_and_one_not_matching_item(number, wordlist, expected):
+    phonewords_maker = phonewords()
+    assert phonewords_maker.list_words(number, wordlist) == expected 
+
+
+input_sample = [
+    ("23", ["b", "d"], ["b", "d"]),
+]
+
+@pytest.mark.parametrize("number,wordlist,expected", input_sample)
+def test_phonewords_should_return_both_items_when_numstring_is_two_digits_and_word_list_contains_two_items_one_matching_each_number(number, wordlist, expected):
     phonewords_maker = phonewords()
     assert phonewords_maker.list_words(number, wordlist) == expected 
